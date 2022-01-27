87b604ed93c38d84896cb80b07235f08852a2ed1
Adam <inmon.adam@yahoo.com>
2022-01-26 19:42:31 -0600

    commit tree b55bd72739cbc900747bd2563964c247efdebae8

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -1,1 +1,38 @@
 
+class CashRegister
+
+    attr_accessor :total, :employee_discount, :items, :last_transaction
+
+    def initialize(employee_discount = 0)
+        @total = 0
+        @employee_discount = employee_discount
+        @items = []
+    end
+
+    def total
+        @total
+    end
+
+    def add_item(title, price, quantity = 1)
+        @last_transaction = (price * quantity)
+        @total += @last_transaction
+        quantity.times do 
+            self.items << title
+        end
+    end 
+
+    def apply_discount
+        if (self.employee_discount != 0)
+            percentage = (100 - self.employee_discount.to_f) / 100
+            @total = (@total * percentage).to_i
+            "After the discount, the total comes to $#{@total}."
+        else
+            "There is no discount to apply."
+        end
+    end
+
+    def void_last_transaction
+        @total -= @last_transaction
+    end
+
+end
